79aecea3b1e1acc92250768f67165155b1efe158
XXXMrG <mrgitservice@outlook.com>
2020-03-18 20:42:58 +0800

    chore: 🧐 header component v1.0 & try write dropdown

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -9,26 +9,22 @@ import styles from './index.module.css';
 import Example from './examples';
 import { getScrollTop } from '../util/scroll';
 const App = () => {
-  const navLayout = [
-    { i: 'logo', x: 3, y: 0, w: 1, h: 2, static: true },
-    { i: 'title', x: 4, y: 0, w: 4, h: 2, static: true },
-  ];
   const headLayout = [
+    { i: 'logo', x: 0, y: 0.25, w: 1, h: 1.5, static: true },
+    { i: 'title', x: 1, y: 0, w: 2, h: 2, static: true },
     { i: '1', x: 4, y: 0, w: 1, h: 2, static: true },
     { i: '2', x: 5.5, y: 0, w: 1, h: 2, static: true },
     { i: '3', x: 7, y: 0, w: 1, h: 2, static: true },
   ];
 
-  const navRef = useRef(null);
   const headRef = useRef(null);
 
   useEffect(() => {
-    const navNode = navRef.current;
     const headNode = headRef.current;
-    window.addEventListener('scroll', e => {
-      if (navNode.offsetHeight > getScrollTop()) {
+    window.addEventListener('scroll', () => {
+      if (headNode.offsetHeight > getScrollTop()) {
         headNode.classList.remove(styles.sticky);
-        console.log(headNode.className);
+        // console.log(headNode.className);
       } else {
         headNode.classList.add(styles.sticky);
       }
@@ -36,37 +32,43 @@ const App = () => {
   }, []);
   return (
     <div>
-      <nav ref={navRef}>
-        <GridLayout
-          compactType="horizontal"
-          className="layout"
-          layout={navLayout}
-          cols={12}
-          rowHeight={15}
-          width={1200}
-          margin={[20, 5]}
-        >
-          <div key="logo" className={styles.logobox}>
-            <img src={logo} className={styles.logo}></img>
-          </div>
-          <div className={styles.item} key="title">
-            Recursive Animtation
-          </div>
-        </GridLayout>
-      </nav>
       <div className={styles.header} ref={headRef}>
         <GridLayout
           compactType="horizontal"
           className="layout"
           layout={headLayout}
           cols={12}
-          rowHeight={20}
+          rowHeight={30}
           width={1200}
           margin={[20, 0]}
         >
-          <div className={styles.itemLink} key="1"></div>
-          <div className={styles.itemLink} key="2"></div>
-          <div className={styles.itemLink} key="3"></div>
+          <div key="logo" className={styles.logobox}>
+            <img src={logo} className={styles.logo}></img>
+          </div>
+          <div key="title">
+            <p className={styles.title}>Recursive Animtation</p>
+          </div>
+          <div key="1">
+            <div className={styles.itemLink}>
+              <a className={styles.link} href="#">
+                Guide
+              </a>
+            </div>
+          </div>
+          <div key="2">
+            <div className={styles.itemLink}>
+              <a className={styles.link} href="#">
+                Examples
+              </a>
+            </div>
+          </div>
+          <div key="3">
+            <div className={styles.itemLink}>
+              <a className={styles.link} href="#">
+                Playground
+              </a>
+            </div>
+          </div>
         </GridLayout>
       </div>
 
