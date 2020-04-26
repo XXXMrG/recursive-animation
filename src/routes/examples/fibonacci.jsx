af3e95b4a7758d9dcaeaf6af9706cdd19bdf83ae
XXXMrG <mrgitservice@outlook.com>
2020-04-26 22:08:59 +0800

    feat(index): 💄  add new favicon

--- src/routes/examples/fibonacci.jsx
+++ src/routes/examples/fibonacci.jsx
@@ -9,18 +9,21 @@ const ResponsiveGridLayout = WidthProvider(Responsive);
 
 const Fibonacci = () => {
   const [code, setCode] = useState('');
-  const defaultValue = `function function_name(argument) {
-    // body...
-}`;
+  const [deep, setDeep] = useState(3);
+  const defaultValue = `function fibonacci(num) {
+    if (num <= 1) {
+        return num;
+    }
+    const result = fibonacci(num - 1) + fibonacci(num - 2);
+    return result;
+ }`;
   const layout = {
     lg: [
-      { i: 'canvas', x: 6, y: 0, w: 4.5, h: 4.5, isResizable: false },
-      { i: 'editor', x: 0, y: 0, w: 5, h: 6, static: true },
+      { i: 'canvas', x: 6, y: 1, w: 4.5, h: 4.5, isResizable: false },
+      { i: 'editor', x: 0, y: 1, w: 5, h: 3, static: true },
+      { i: 'deep', x: 0, y: 0, w: 2, h: 1, static: true },
     ],
   };
-  useEffect(() => {
-    console.log(code);
-  });
   return (
     <ResponsiveGridLayout
       layouts={layout}
@@ -29,15 +32,30 @@ const Fibonacci = () => {
       style={{ boxSizing: 'border-box' }}
       containerPadding={[10, 25]}
     >
-      <div key="canvas" className="zi-card" style={{ padding: 0 }}>
-        <div className={styles.card}>
-          <Fibtree number={5} />
+      <div key="deep">
+        <div className="zi-select-container">
+          <select
+            className="zi-select"
+            value={deep}
+            onChange={e => {
+              setDeep(e.target.value);
+            }}
+          >
+            <option value={3}>3</option>
+            <option value={4}>4</option>
+            <option value={5}>5</option>
+          </select>
+          <i className="arrow zi-icon-up"></i>
         </div>
       </div>
-
       <div key="editor" className="zi-card zi-dark" style={{ padding: '1px' }}>
         <div className={styles.card}>
           <Ace onChange={setCode} defaultValue={defaultValue} />
+        </div>
+      </div>
+      <div key="canvas" className="zi-card" style={{ padding: 0 }}>
+        <div className={styles.card}>
+          <Fibtree number={5} />
         </div>
       </div>
     </ResponsiveGridLayout>
